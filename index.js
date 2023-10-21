f5c9a7cf20d978491aa7ef91bff0a04c0fb76d11
sai36601 <saikumar36601@gmail.com>
2023-10-21 07:10:02 +0000

    Commit message

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  // Write your code here.
+  <div className="container">
+    <div>
+      <h1>Congratulations</h1>
+    </div>
+    <div className="profilecard">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Kiran V</h1>
+      <p>Vishnu institute of Computer Education and Technology, Bhimavaram.</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
